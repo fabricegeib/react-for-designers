212ed706023353dc63be35833beb4f68180e7f9b
Fabrice Geib <fabricegeib@gmail.com>
2018-09-24 21:04:56 +0200

    first modification

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learn to <br />design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
