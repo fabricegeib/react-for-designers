f98f523aa3dda91a7bef84fa03afd7197193ca9a
Fabrice Geib <fabricegeib@gmail.com>
2018-09-30 22:17:34 +0200

    state for component : scroll supérieur à 50px ajoute une class au Header

--- src/components/header.js
+++ src/components/header.js
@@ -4,19 +4,42 @@ import './Header.css'
 // import logo from '../images/logo-designcode.svg'
 
 class Header extends React.Component {
-    render() {
-    return (
-      <div className="Header">
-        <div className="HeaderGroup">
-          <Link to="/"><img src={require ('../images/logo-designcode.svg')} width="30" alt="logo" /></Link>
-          <Link to="/courses">Courses</Link>
-          <Link to="/downloads">Downloads</Link>
-          <Link to="/workshops">Workshops</Link>
-          <Link to="/buy"><button>Buy</button></Link>
-        </div>
-      </div>
-    )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
     }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll',
+    this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true})
+    } else {
+      this.setState({ hasScrolled: false})
+    }
+  }
+
+  render() {
+  return (
+    <div className={this.state.hasScrolled ? ' Header HeaderScrolled' : 'Header'}>
+      <div className="HeaderGroup">
+        <Link to="/"><img src={require ('../images/logo-designcode.svg')} width="30" alt="logo" /></Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/downloads">Downloads</Link>
+        <Link to="/workshops">Workshops</Link>
+        <Link to="/buy"><button>Buy</button></Link>
+      </div>
+    </div>
+  )
+  }
 }
 
 export default Header
