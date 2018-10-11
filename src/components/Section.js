f4563adc6f3d72820f8304f808e345fc5e444b42
Fabrice Geib <fabricegeib@gmail.com>
2018-10-11 19:47:45 +0200

    styled components

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,6 +9,11 @@ const SectionGroup = styled.div`
     display: grid;
     grid-template-rows: 300px auto;
     grid-gap: 20px;
+    position: relative;
+
+    @media (max-width: 640px) {
+        height 820px;
+    }
 `
 
 const SectionLogo = styled.img`
@@ -34,20 +39,38 @@ const SectionTitle = styled.h3`
     font-size: 60px;
     margin: 0;
     line-height: 1.2;
+
+    @media (max-width: 720px) {
+        font-size: 40px;
+    }
 `
 
 const SectionText = styled.p`
     color: white;
 `
 
+const WaveBottom = styled.div`
+    position: absolute;
+    width: 100%;
+    bottom: -6px;
+`
+
+const WaveTop = styled.div`
+    position: absolute;
+    width: 100%;
+    top: -6px;
+    transform: rotate(180deg);
+`
+
 const Section = props => (
     <SectionGroup image={props.image}>
+        <WaveTop><Wave /></WaveTop>
+        <WaveBottom><Wave /></WaveBottom>
         <SectionLogo src={props.logo} />
         <SectionTitleGroup>
             <SectionTitle>{props.title}</SectionTitle>
             <SectionText>{props.text}</SectionText>
         </SectionTitleGroup>
-        <Wave />
     </SectionGroup>
 )
 
