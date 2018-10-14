99e089436ddad7767385e15b7774cabdf60e9d91
Fabrice Geib <fabricegeib@gmail.com>
2018-10-14 20:04:19 +0200

    react-static-data-json 2

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,12 +2,25 @@ import React from 'react'
 import styled from 'styled-components'
 
 const CellGroup = styled.div`
+    display: grid;
+    grid-template-columns: 60px auto;
+    grid-gap: 25px;
+    align-items: center;
 `
 
 const CellImage = styled.div`
+    width: 60px;
+    height: 60px;
+    background: black;
+    border-radius: 10px;
+    background-image: url(${props => props.image});
+    background-size: 60px;
 `
 
 const CellTitle = styled.div`
+    font-size: 24px;
+    border-bottom: 1px solid rgba(0,0,0, 0.1);
+    padding: 30px;
 `
 
 const Cell = props => (
@@ -16,3 +29,5 @@ const Cell = props => (
         <CellTitle>{props.title}</CellTitle>
     </CellGroup>
 )
+
+export default Cell
